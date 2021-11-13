2a79937853d4f3a311a8332684eea13e82bb335b
Dareeben13 <e2kelly2000@gmail.com>
2021-11-13 13:31:16 +0100

    fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users
-// @desc   Test route
+const User = require("../../models/User");
+
+// @route   POST api/users
+// @desc   Register user
 // @access  Public
 
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please enter a password with six or more characters").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+
+    res.send("User route");
+  }
+);
 
 module.exports = router;
